f0aee90e06872922550e1ceac3502a408b6d4488
mboudet <mateo.boudet@irisa.fr>
2019-07-04 09:32:41 +0000

    various fixes & tests

--- toxsign/projects/tests/test_views.py
+++ toxsign/projects/tests/test_views.py
@@ -4,10 +4,13 @@ from django.urls import reverse, resolve
 from django.test import Client
 
 from toxsign.projects.tests.factories import ProjectFactory
+from toxsign.users.tests.factories import UserFactory
+from toxsign.projects.views import EditView
+
 
 pytestmark = pytest.mark.django_db
 
-def test_list():
+def test_list_view(client):
     project = ProjectFactory.create()
     client = Client()
     response = client.get(reverse('projects:index'))
@@ -16,18 +19,30 @@ def test_list():
     new_project = projects[0]
     assert new_project.name == project.name
 
-def test_details():
+def test_details_view(client):
     project = ProjectFactory.create()
-    client = Client()
     response = client.get(reverse("projects:detail", kwargs={"prjid": project.tsx_id}))
     response_project = projects = response.context['project']
     assert project == response_project
 
-def test_update():
+def test_update_anonymous(client):
     project = ProjectFactory.create()
-    client = Client()
-    body = {'name' : project.context_object_name, 'description': "My new description"}
-    response = self.client.post(reverse("projects:project_edit", kwargs={"pk": project.id}), body)
+    new_description = project.description + '_new'
+    body = {'name': project.name, 'description': new_description}
+    # This actually fails silently.. with a redirect.
+    response = client.post(reverse("projects:project_edit", kwargs={"pk": project.id}), body)
     assert response.status_code == 302
     project.refresh_from_db()
-    assert project.description == "My new description"
+    assert project.description != new_description
+
+def test_update_logged(client, django_user_model):
+    # Need to be logged for this one
+    user = django_user_model.objects.create_user(username='random', password='user')
+    client.login(username='random', password='user')
+    project = ProjectFactory.create(created_by=user)
+    new_description = project.description + '_new'
+    body = {'name': project.name, 'description': new_description}
+    response = client.post(reverse("projects:project_edit", kwargs={"pk": project.id}), body)
+    assert response.status_code == 302
+    project.refresh_from_db()
+    assert project.description == new_description
