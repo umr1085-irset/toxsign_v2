e9dfc1bb66c23f0f15d12c330ea2e7fb8b3b7c7d
mboudet <mateo.boudet@gmail.com>
2019-07-04 09:37:46 +0200

    test update view

--- toxsign/projects/tests/test_views.py
+++ toxsign/projects/tests/test_views.py
@@ -22,3 +22,12 @@ def test_details():
     response = client.get(reverse("projects:detail", kwargs={"prjid": project.tsx_id}))
     response_project = projects = response.context['project']
     assert project == response_project
+
+def test_update():
+    project = ProjectFactory.create()
+    client = Client()
+    body = {'name' : project.context_object_name, 'description': "My new description"}
+    response = self.client.post(reverse("projects:project_edit", kwargs={"pk": project.id}), body)
+    assert response.status_code == 302
+    project.refresh_from_db()
+    assert project.description == "My new description"
