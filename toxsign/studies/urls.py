e700fae2727db14ba7e600aef0bc68bdf41c6c28
mboudet <mateo.boudet@irisa.fr>
2019-07-05 14:56:42 +0000

    Small fixes

--- toxsign/studies/urls.py
+++ toxsign/studies/urls.py
@@ -4,6 +4,6 @@ from toxsign.studies import views
 app_name = "studies"
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    path('<str:stdid>/', views.DetailView, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('study_edit/<int:pk>/', views.EditView.as_view(), name='study_edit'),
 ]
