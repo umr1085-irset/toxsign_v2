d0e91c1bf3f4a826164ae49e8562e61f5e1053aa
mboudet <mateo.boudet@gmail.com>
2019-07-05 16:37:45 +0200

    First implementation

--- config/views.py
+++ config/views.py
@@ -12,10 +12,7 @@ import json
 
 from toxsign.projects.models import Project
 from toxsign.studies.models import Study
-
-
-
-
+from toxsign.signatures.models import Signature
 
 def HomeView(request):
         context = {}
@@ -23,10 +20,19 @@ def HomeView(request):
 
 def autocompleteModel(request):
     query = request.GET.get('q')
-    results = Project.objects.filter(Q(name__icontains=query) | Q(description__icontains=query) | Q(tsx_id__icontains=query))
-
+    results_projects = Project.objects.filter(Q(name__icontains=query) | Q(description__icontains=query) | Q(tsx_id__icontains=query))
+    results_studies = Study.objects.filter(Q(name__icontains=query) | Q(description__icontains=query) | Q(tsx_id__icontains=query))
+    result_signatures = SignatureStudy.objects.filter(Q(name__icontains=query) | Q(description__icontains=query) | Q(tsx_id__icontains=query))
+    results = {
+        'projects_number' : len(results_projects),
+        'studies_number' : len(results_studies),
+        'signatures_number' : len(results_signatures),
+        'projects': results_projects,
+        'studies': results_studies,
+        'signatures': result_signatures
+    }
     return render(request, 'pages/ajax_search.html', {'statuss': results})
-    
+
 def search(request,query):
     print(query)
     search_qs = Project.objects.filter(name__contains=query)
