661d2bd54b8f86a42118f41b8d1833bea019ed4a
mboudet <mateo.boudet@irisa.fr>
2019-07-09 12:51:13 +0000

    Fixesé

--- toxsign/studies/urls.py
+++ toxsign/studies/urls.py
@@ -3,6 +3,6 @@ from toxsign.studies import views
 
 app_name = "studies"
 urlpatterns = [
-    path('<str:stdid>/', views.DetailView.as_view(), name='detail'),
+    path('<str:stdid>/', views.DetailView, name='detail'),
     path('study_edit/<int:pk>/', views.EditView.as_view(), name='study_edit'),
 ]
