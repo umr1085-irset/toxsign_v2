cbcafcca17eca28384d5b57e32698453c18c53b0
mboudet <mateo.boudet@irisa.fr>
2019-07-09 13:51:25 +0000

    Removed status for non project entities 2

--- toxsign/studies/admin.py
+++ toxsign/studies/admin.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 from toxsign.studies.models import Study
 class StudyAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['name', 'created_by', 'status', 'description', 'study_type', 'experimental_design', 'results', 'project']}),
+        (None,               {'fields': ['name', 'created_by', 'description', 'study_type', 'experimental_design', 'results', 'project']}),
     ]
     list_display = ['name', 'created_at', 'updated_at']
     search_fields = ['name']
--- toxsign/studies/models.py
+++ toxsign/studies/models.py
@@ -10,11 +10,6 @@ from toxsign.projects.models import Project
 
 
 class Study(models.Model):
-    AVAILABLE_STATUS = (
-        ('PRIVATE', 'Private'),
-        ('PENDING', 'Pending'),
-        ('PUBLIC', 'Public'),
-    )
     STUDY_TYPE = (
         ('INTERVENTIONAL', 'Interventional'),
         ('OBSERVATIONAL', 'Observational'),
@@ -25,7 +20,6 @@ class Study(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     created_by = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.CASCADE, related_name='%(app_label)s_%(class)s_created_by')
     updated_at = models.DateTimeField(auto_now=True, null=True, verbose_name=("user"))
-    status = models.CharField(max_length=20, choices=AVAILABLE_STATUS, default="PRIVATE")
     description = models.TextField("Description")
     experimental_design = models.TextField("Experimental design")
     study_type = models.CharField(max_length=50, choices=STUDY_TYPE, default="INTERVENTIONAL")
