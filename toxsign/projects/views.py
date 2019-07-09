fd558587e6c72814f6bc8be8198ec00275801afe
mboudet <mateo.boudet@irisa.fr>
2019-07-09 11:30:39 +0000

    Fix & cleaner syntax)

--- toxsign/projects/views.py
+++ toxsign/projects/views.py
@@ -7,9 +7,10 @@ from django.views import generic
 from django.views.generic import DetailView, ListView, RedirectView, UpdateView
 from django.shortcuts import redirect
 
+from toxsign.assays.models import Assay
 from toxsign.projects.models import Project
-from toxsign.projects.forms import ProjectForm
 from toxsign.studies.models import Study
+from toxsign.signatures.models import Signature
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
@@ -17,14 +18,8 @@ def DetailView(request, prjid):
     project_object = Project.objects.get(tsx_id=prjid)
     project = get_object_or_404(Project, pk=project_object.id)
     studies = project.study_of.all()
-    assays = []
-    for study in studies:
-        assays = assays + [assay for assay in study.assay_of.all()]
-    signatures = []
-    for assay in assays:
-        for factor in assay.factor_of.all():
-            signatures = signatures + [factor for factor in factor.signature_of_of.all()]
-
+    assays = Assay.objects.filter(study__project=project)
+    signatures = Signature.objects.filter(factor__assay__study__project=project)
     return render(request, 'projects/details.html', {'project': project,'studies': studies, 'assays': assays, 'signatures': signatures})
 
 class EditView(LoginRequiredMixin, UpdateView):
