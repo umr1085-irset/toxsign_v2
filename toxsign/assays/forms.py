88670d4bd8f8495b83fce639fd51bcec5c0c1143
mboudet <mateo.boudet@gmail.com>
2019-07-11 10:43:02 +0200

    Autocomplete in assay & factors

--- toxsign/assays/forms.py
+++ toxsign/assays/forms.py
@@ -1,12 +1,29 @@
 from django import forms
+from dal import autocomplete
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout
 from crispy_forms.bootstrap import FormActions
 from toxsign.assays.models import Assay, Factor
-
+from toxsign.ontologies.models import Cell, CellLine, Chemical, Species, Tissue
 
 
 class AssayCreateForm(forms.ModelForm):
+
+        cell_line = forms.ModelChoiceField(
+                    queryset=CellLine.objects.all(),
+                    widget=autocomplete.ModelSelect2(url='/ontologies/cellline-autocomplete')
+                  )
+        cell = forms.ModelChoiceField(
+                    queryset=Cell.objects.all(),
+                    widget=autocomplete.ModelSelect2(url='/ontologies/cell-autocomplete')
+                  )
+        organism = forms.ModelChoiceField(
+                    queryset=Species.objects.all(),
+                    widget=autocomplete.ModelSelect2(url='/ontologies/species-autocomplete')
+                  )
+        tissue = forms.ModelChoiceField(
+                    queryset=Tissue.objects.all(),
+                    widget=autocomplete.ModelSelect2(url='/ontologies/tissue-autocomplete')
 
     class Meta:
         model = Assay
@@ -19,6 +36,11 @@ class AssayCreateForm(forms.ModelForm):
         self.helper.add_input(Submit('save', 'Save'))
 
 class FactorCreateForm(forms.ModelForm):
+
+    chemical = forms.ModelChoiceField(
+                queryset=Chemical.objects.all(),
+                widget=autocomplete.ModelSelect2(url='/ontologies/chemical-autocomplete')
+              )
 
     class Meta:
         model = Factor
