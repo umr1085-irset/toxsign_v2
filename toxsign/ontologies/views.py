9165624dfcb6ce419a4b36a113f3471af4a23b2c
mboudet <mateo.boudet@gmail.com>
2019-07-11 10:24:17 +0200

    Autocomplete for all?

--- toxsign/ontologies/views.py
+++ toxsign/ontologies/views.py
@@ -1,16 +1,44 @@
 from dal import autocomplete
 from django.db.models import Q
-from toxsign.ontologies.models import Disease
+from toxsign.ontologies.models import *
 
-class DiseaseAutocomplete(autocomplete.Select2QuerySetView):
+class OntologyAutocomplete(autocomplete.Select2QuerySetView):
+
+    def __init__(self, model):
+        self.model = model
 
     def get_queryset(self):
-
         if not self.request.user.is_authenticated():
-                    return Disease.objects.none()
-
-        qs = Disease.objects.all()
+                    return self.model.objects.none()
+        qs = self.model.objects.all()
         if self.q:
             qs = qs.filter(Q(name__istartswith=self.q))
-
         return qs
+
+class BiologicalAutocomplete(OntologyAutocomplete):
+    def __init__(self):
+        super(BiologicalAutocomplete, self).__init__(Biological)
+
+class CellLineAutocomplete(OntologyAutocomplete):
+    def __init__(self):
+        super(CellLineAutocomplete, self).__init__(CellLine)
+
+class ChemicalAutocomplete(OntologyAutocomplete):
+    def __init__(self):
+        super(ChemicalAutocomplete, self).__init__(Chemical)
+
+class DiseaseAutocomplete(OntologyAutocomplete):
+    def __init__(self):
+        super(DiseaseAutocomplete, self).__init__(Disease)
+
+class ExperimentAutocomplete(OntologyAutocomplete):
+    def __init__(self):
+        super(ExperimentAutocomplete, self).__init__(Experiment)
+
+class SpeciesAutocomplete(OntologyAutocomplete):
+    def __init__(self):
+        super(SpeciesAutocomplete, self).__init__(Species)
+
+class TissueAutocomplete(OntologyAutocomplete):
+    def __init__(self):
+        super(TissueAutocomplete, self).__init__(Tissue)
