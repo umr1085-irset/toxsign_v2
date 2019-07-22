d915a52d41b1a0d9756aba4f75b84a60ac09e5c5
mboudet <mateo.boudet@irisa.fr>
2019-07-22 08:49:52 +0000

    no need for this

--- toxsign/projects/views.py
+++ toxsign/projects/views.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
