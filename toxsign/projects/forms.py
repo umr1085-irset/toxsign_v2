9498ad45dc30eb39bee9a28df4c9ad8950797053
mboudet <mateo.boudet@irisa.fr>
2019-07-30 13:22:43 +0000

    Form validation for permission type & typo fix

--- toxsign/projects/forms.py
+++ toxsign/projects/forms.py
@@ -3,6 +3,8 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout
 from crispy_forms.bootstrap import FormActions
 
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from toxsign.projects.models import Project
 
 class ProjectCreateForm(forms.ModelForm):
@@ -20,14 +22,27 @@ class ProjectCreateForm(forms.ModelForm):
         self.user = kwargs.pop('user')
 
         super(ProjectCreateForm, self).__init__(*args, **kwargs)
-        self.fields['read_groups'].help_text = "Groups allowed to view the project and all related entities. Will be ignored if the visibility is set to public"
-        self.fields['edit_groups'].help_text = "Groups allowed to edit the project and all related entities. Should be a subset of the groups with view permissions"
+        self.fields['read_groups'].help_text = "Groups with viewing permission on project and subentities. Will be ignored if the visibility is set to public. Use 'ctrl' to select multiple/unselect."
+        self.fields['edit_groups'].help_text = "Groups with editing permission on project and subentities. Use 'ctrl' to select multiple/unselect."
 
         # TODO : Give link to group creation interface?
-        groups = self.user.groups.values_list('name',flat=True)
+        groups = self.user.groups.all()
         self.fields['read_groups'].queryset = groups
         self.fields['edit_groups'].queryset = groups
 
         self.helper = FormHelper(self)
         self.helper.form_method = 'POST'
         self.helper.add_input(Submit('save', 'Save'))
+
+
+    def is_valid(self):
+        valid = super(ProjectCreateForm, self).is_valid()
+        if not valid:
+            return valid
+
+        for edit_group in self.cleaned_data['edit_groups']:
+            if edit_group not in self.cleaned_data['read_groups']:
+                self.add_error("edit_groups", ValidationError(_('Projects with editing rights must also have reading rights'), code='invalid'))
+                return False
+
+        return True
