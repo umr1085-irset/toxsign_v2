048732d9bdc4febecfcf59062e509584db881613
mboudet <mateo.boudet@gmail.com>
2019-12-11 10:43:54 +0000

    update

--- toxsign/scripts/data.py
+++ toxsign/scripts/data.py
@@ -25,7 +25,7 @@ def prepare_homolog_data(self, force=False):
     ]
 
     # Should load this from config file maybe
-    dest_dir = "/app/tools/admin_data/"
+    dest_dir = "/app/toxsign/media/jobs/admin/"
     _download_datafiles(dest_dir, urls, force=force)
 
     if os.path.exists(os.path.join(dest_dir, "annotation")) and not force:
@@ -81,14 +81,14 @@ def index_genes(signature, new_path, new_unix_path):
     signature.down_gene_file_path.name = new_path + "down_genes.txt"
     signature.interrogated_gene_file_path.name = new_path + "all_genes.txt"
 
-    gene_dict = _generate_values(signature)
+    gene_dict, count = _generate_values(signature)
     signature.expression_values = _format_values(gene_dict)
     _write_gene_file(gene_dict, new_unix_path + signature.tsx_id + ".sign")
     signature.expression_values_file.name = new_path + signature.tsx_id + ".sign"
 
-    signature.up_gene_number = _count_lines(new_unix_path + "up_genes.txt")
-    signature.down_gene_number = _count_lines(new_unix_path + "down_genes.txt")
-    signature.interrogated_gene_number = _count_lines(new_unix_path + "all_genes.txt")
+    signature.up_gene_number = count['up']
+    signature.down_gene_number = count['down']
+    signature.interrogated_gene_number = count['all']
 
     return signature
 
@@ -99,20 +99,21 @@ def move_files(signature, new_path, new_unix_path):
     return signature
 
 @app.task(bind=True)
-def change_status(self, project_id):
+def change_status(self, project_id=None):
     # Import here to avoid cyclical import
     from toxsign.projects.models import Project
     from toxsign.signatures.models import Signature
-    temp_dir_path = "/app/tools/job_dir/temp/" + self.request.id + "/"
+    temp_dir_path = "/app/toxsign/media/jobs/temp/" + self.request.id + "/"
 
     if os.path.exists(temp_dir_path):
         print("Folder {} already exists: stopping..".format(temp_dir_path))
         return
 
     # Should test if this project has signature. No point in recalculating if nothing is new
-    project_sig = Signature.objects.filter(factor__assay__project__id=project_id)
-    if not project_sig.exists():
-        return
+    if project_id:
+        project_sig = Signature.objects.filter(factor__assay__project__id=project_id)
+        if not project_sig.exists():
+            return
 
     public_sigs = Signature.objects.filter(factor__assay__project__status="PUBLIC")
     if not public_sigs.exists():
@@ -121,49 +122,52 @@ def change_status(self, project_id):
     os.mkdir(temp_dir_path)
 
     for sig in public_sigs:
-        if sig.expression_values_file:
+        if sig.expression_values_file and os.path.exists(sig.expression_values_file.path):
             shutil.copy2(sig.expression_values_file.path, temp_dir_path)
-    if os.path.exists("/app/tools/admin_data/public.RData"):
-        shutil.copy2("/app/tools/admin_data/public.RData", temp_dir_path + "public.RData.old")
+    if os.path.exists("/app/toxsign/media/jobs/admin/public.RData"):
+        shutil.copy2("/app/toxsign/media/jobs/admin/public.RData", temp_dir_path + "public.RData.old")
 
     # Need to check the result...
     subprocess.run(['/bin/bash', '/app/tools/make_public/make_public.sh', temp_dir_path])
 
 
-def _count_lines(path):
-    with open(path) as f:
-        for i, l in enumerate(f):
-            pass
-    return i + 1
-
 def _generate_values(signature):
     # Starts from scratch
     values = {}
+    count = {
+        'up': 0,
+        'down': 0,
+        'all': 0
+    }
     gene_type = signature.gene_id
     # Starts with interrogated file to get them all (we will upload them later)
     if signature.interrogated_gene_file_path:
-        values = _prepare_values(values, signature.interrogated_gene_file_path.path, gene_type)
+        values, count['all'] = _prepare_values(values, signature.interrogated_gene_file_path.path, gene_type)
     if signature.up_gene_file_path:
-        values = _extract_values(values, signature.up_gene_file_path.path, gene_type, "1")
+        values, count['up'] = _extract_values(values, signature.up_gene_file_path.path, gene_type, "1")
     if signature.down_gene_file_path:
-        values = _extract_values(values, signature.down_gene_file_path.path, gene_type, "-1")
-    return values
+        values, count['down'] = _extract_values(values, signature.down_gene_file_path.path, gene_type, "-1")
+    return values, count
 
 def _extract_values(values, file, gene_type, expression_value=None):
 
-    genes = set()
+    count = 0
     if not os.path.exists(file):
-        return values
+        return values, count
+
     with open(file, 'r') as f:
         for line in f:
             gene_id = line.split("\t")[0].strip()
+            if not gene_id:
+                continue
+            count += 1
             # Shoud not happen, but just in case
             if not gene_id in values:
                 values[gene_id] = {'value': expression_value, 'homolog_id': 'NA', 'gene_name':"NA", 'in_base': 0}
             else:
                 values[gene_id]['value'] = expression_value
 
-    return values
+    return values, count
 
 def _prepare_values(values, file, gene_type):
 
@@ -171,7 +175,7 @@ def _prepare_values(values, file, gene_type):
     processed_genes = set()
 
     if not os.path.exists(file):
-        return values
+        return values, 0
 
     with open(file, 'r') as f:
         for line in f:
@@ -193,7 +197,7 @@ def _prepare_values(values, file, gene_type):
         for missing_gene in genes - processed_genes:
             values[missing_gene] = {'value': 0, 'homolog_id': 'NA', 'gene_name':"NA", 'in_base': 0}
 
-    return values
+    return values, len(genes)
 
 def _write_gene_file(gene_values, path):
 
--- toxsign/scripts/processing.py
+++ toxsign/scripts/processing.py
@@ -31,11 +31,11 @@ def run_distance(self, signature_id, user_id=None):
     task_id = self.request.id + "_" + str(ztime)
 
     # Does it work with no rData file? Using only provided signatures?
-    if not os.path.exists("/app/tools/admin_data/public.RData"):
+    if not os.path.exists("/app/toxsign/media/jobs/admin/public.RData"):
         logger.exception("No public.RData file. Stopping..")
         raise TaskFailure('No public.RData file')
 
-    job_dir_path = "/app/tools/job_dir/results/" + task_id + "/"
+    job_dir_path = "/app/toxsign/media/jobs/results/" + task_id + "/"
 
     if os.path.exists(job_dir_path):
         logger.exception("Result directory {} already exists. Stopping.".format(task_id))
@@ -47,7 +47,7 @@ def run_distance(self, signature_id, user_id=None):
     signatures = []
     if user_id:
         user = User.objects.get(id=user_id)
-        accessible_projects = [project for project in Project.objects.all() if check_view_permissions(user, project, strict=True)]
+        accessible_projects = [project for project in Project.objects.exclude(status="PUBLIC") if check_view_permissions(user, project, strict=True, allow_superuser=False)]
         signatures = Signature.objects.filter(factor__assay__project__in=accessible_projects)
 
     selected_signature = Signature.objects.get(id=signature_id)
@@ -94,11 +94,11 @@ def run_enrich(self, signature_id):
     task_id = self.request.id + "_" + str(ztime)
 
     # Does it work with no rData file? Using only provided signatures?
-    if not os.path.exists("/app/tools/admin_data/annotation"):
+    if not os.path.exists("/app/toxsign/media/jobs/admin/annotation"):
         logger.exception("No annotation file. Stopping..")
         raise TaskFailure('No annotation file')
 
-    job_dir_path = "/app/tools/job_dir/results/" + task_id + "/"
+    job_dir_path = "/app/toxsign/media/jobs/results/" + task_id + "/"
 
     if os.path.exists(job_dir_path):
         logger.exception("Result directory {} already exists. Stopping.".format(task_id))
@@ -126,7 +126,7 @@ def run_enrich(self, signature_id):
         raise TaskFailure('Error running bash script')
 
     # Update job with results
-    zip_path = _zip_results(job_dir_path)
+    zip_path = zip_results(job_dir_path)
 
     results = {
         'files': [
@@ -146,7 +146,7 @@ def run_enrich(self, signature_id):
 
 def _prepare_temp_folder(request_id, signature, additional_signatures=[], add_RData=False, add_Homolog=False):
 
-    temp_dir_path =  "/app/tools/job_dir/temp/" + request_id + "/"
+    temp_dir_path =  "/app/toxsign/media/jobs/temp/" + request_id + "/"
 
     # Task id SHOUlD be unique, and we SHOUld cleanup afterward..
     if os.path.exists(temp_dir_path):
@@ -156,13 +156,13 @@ def _prepare_temp_folder(request_id, signature, additional_signatures=[], add_RD
 
     shutil.copy2(signature.expression_values_file.path, temp_dir_path)
     for sig in additional_signatures:
-        if sig.expression_values_file:
+        if sig.expression_values_file and os.path.exists(sig.expression_values_file.path):
             shutil.copy2(sig.expression_values_file.path, temp_dir_path)
 
     if add_RData:
-        shutil.copy2('/app/tools/admin_data/public.RData', temp_dir_path)
+        shutil.copy2('/app/toxsign/media/jobs/admin/public.RData', temp_dir_path)
     if add_Homolog:
-        shutil.copy2('/app/tools/admin_data/annotation', temp_dir_path)
+        shutil.copy2('/app/toxsign/media/jobs/admin/annotation', temp_dir_path)
 
     return temp_dir_path
 
