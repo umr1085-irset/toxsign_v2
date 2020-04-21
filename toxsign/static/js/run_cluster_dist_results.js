4f60ed84a5de7248ff09f2d186b42b886665a1f3
mboudet <mateo.boudet@gmail.com>
2020-04-21 12:17:17 +0000

    Last layout changes

--- toxsign/static/js/run_cluster_dist_results.js
+++ toxsign/static/js/run_cluster_dist_results.js
@@ -1,4 +1,26 @@
 $(function () {
 
-    var table = $('#results').DataTable();
+    var table = $('#results').DataTable({
+        "pageLength": 5,
+        "scrollX": true
+    });
+
+    var loadForm = function () {
+      var btn = $(this);
+      $.ajax({
+        url: btn.attr("data-url"),
+        type: 'get',
+        dataType: 'json',
+        beforeSend: function () {
+          $("#modal-group").modal("show");
+        },
+        success: function (data) {
+          $("#modal-group .modal-content").html(data.html);
+        }
+      });
+    };
+
+  /* Binding */
+    $("#results").on("click", ".js-load-modal", loadForm);
+
 });
