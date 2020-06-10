0911568e8d71f98cf43da1d6d0ca76c76f617ff2
mboudet <mateo.boudet@gmail.com>
2020-06-10 12:58:58 +0000

    Removed conditionnal for backup

--- backups/backup_script.sh
+++ backups/backup_script.sh
@@ -11,15 +11,12 @@ LOCAL_BACKUP_DIR="$DIR""/archives/"
 REMOTE_BACKUP_DIR="/groups/irset/archives/web/TOXsIgNv2/"
 DATE=`eval date +%Y%m%d`
 
-if docker-compose -f "$DIR""/../production.yml" exec -e DATABASE_URL=$DATABASE_URL -e CELERY_BROKER_URL=$CELERY_BROKER_URL django python manage.py check_signatures > /dev/null 2>&1; then
-   echo "Writing Postgres dump"
-   docker-compose -f "$DIR""/../production.yml" exec postgres sh -c "pg_dump --clean -U $POSTGRES_USER $POSTGRES_DB > /backups/DB_backup"
-   mv "$DIR""/DB_backup" "$DIR""/../toxsign/media/"
-   echo "Building archive"
-   tar -pzcf  "$LOCAL_BACKUP_DIR""archive-$DATE"".tar.gz" "$DIR""/../toxsign/media/"
-   rm "$DIR""/../toxsign/media/DB_backup"
-fi
-
+echo "Writing Postgres dump"
+docker-compose -f "$DIR""/../production.yml" exec postgres sh -c "pg_dump --clean -U $POSTGRES_USER $POSTGRES_DB > /backups/DB_backup"
+mv "$DIR""/DB_backup" "$DIR""/../toxsign/media/"
+echo "Building archive"
+tar -pzcf  "$LOCAL_BACKUP_DIR""archive-$DATE"".tar.gz" "$DIR""/../toxsign/media/"
+rm "$DIR""/../toxsign/media/DB_backup"
 
 cd $LOCAL_BACKUP_DIR
 ls -1t  | tail -n +4 | xargs rm -f
