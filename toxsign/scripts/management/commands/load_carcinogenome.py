8dfe566dd07b38fff40f035a027f4563cf57f780
Thomas <dardethomas@gmail.com>
2021-10-27 18:01:06 +0200

    Carcinogenome project loading script

--- toxsign/scripts/management/commands/load_carcinogenome.py
+++ toxsign/scripts/management/commands/load_carcinogenome.py
@@ -7,31 +7,64 @@ from django.core.management.base import BaseCommand, CommandError
 from toxsign.superprojects.models import *
 from toxsign.projects.models import *
 from toxsign.assays.models import *
+from toxsign.signatures.models import *
+from toxsign.users.models import *
 
-
-def import_superproject(directory,dict):
+def import_superproject(directory,dict,useremail):
     super_projects = os.listdir(directory)
+    user = User.objects.get(email=useremail)
     for super_project in super_projects :
+        dict_to_insert = {}
         super_project_json = json.loads(os.path.join(directory,super_project))
-        sp = Superproject.objects.create(**super_project_json)
+
+        # Manually mapping each keys thank you Indusha ^^
+        dict_to_insert['name'] = super_project_json['Name']
+        dict_to_insert['contact_mail'] = super_project_json['Contact mail']
+        dict_to_insert['description'] = super_project_json['Description']
+        dict_to_insert['created_by'] = user
+
+        # Create SuperProject from dict_to_insert
+        sp = Superproject.objects.create(**dict_to_insert)
         sp.save()
+
+        # Get SuperProject ID and return data dict 
         dict["super_project"] = sp.id
+        
     return dict
 
-def import_project(directory,dict):
+def import_project(directory,dict,useremail):
     projects = os.listdir(directory)
+    user = User.objects.get(email=useremail)
     super_project = Superproject.objects.get(id=dict["super_project"])
     for project in projects :
         project_json = json.loads(os.path.join(directory,project))
+
         for key in project_json :
             if key not in dict :
                 dict[key]=dict()
+
+            # Manually mapping each keys thank you Indusha ^^ (again)
+            dict_to_insert =  {}
+            dict_to_insert['name'] = project_json['Name']
+            dict_to_insert['description'] = project_json['Description']
+            dict_to_insert['created_by'] = user
+
+            # Add genotox & carcinotox information in description
+            if project_json['Carcinogenicity'] != "NA" :
+                dict_to_insert['description'] = dict_to_insert['description'] + '\n'+ project_json['Carcinogenicity']
+            if project_json['Genotoxicity'] != "NA" :
+                dict_to_insert['description'] = dict_to_insert['description'] + '\n'+ project_json['Genotoxicity']
+            dict_to_insert['project_type'] = project_json['Project type']
+
             p=Project.objects.create(**project_json[key])
             p.superproject = super_project
             p.save()
             dict[key]["project"] = p.id
     return dict
 
+#########################################
+#  WIP 
+#########################################
 def import_assay(directory,dict):
     assays = os.listdir(directory)
     for assay in assays :
@@ -72,6 +105,22 @@ def import_subfactor(directory,dict):
             sf.factor = factor
             sf.save()
             dict[key]["subfactor"] = sf.id
+    return dict
+
+def import_signature(directory,dict):
+    signatures = os.listdir(directory)
+    signatures_file_dir = os.path.join(directory,"sig_files")
+    for signature in signatures :
+        signatures_json = json.loads(os.path.join(directory,signature))
+        for key in signatures_json :
+            if key not in dict :
+                dict[key]=dict()
+            signatures_json['']
+            factor = Factor.objects.get(id=dict[key]["factor"])
+            s=Signature.objects.create(**signatures_json[key])
+            s.factor = factor
+            s.save()
+            dict[key]["signature"] = s.id
     return dict            
 
 
